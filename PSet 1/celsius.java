46758df2c858271f0c2c5c4095ba671d18bf4ce4
Liza :) <lizash605@gmail.com>
2017-01-01 23:15:54 +0200

    Update celsius.java

--- PSet 1/celsius.java
+++ PSet 1/celsius.java
@@ -1,6 +1,6 @@
 double fahrenheit = 50.0;
 double celsius;
 celsius = (fahrenheit - 32) * 5 / 9;
-System.out.println(celcius);
+System.out.println(celsius);
 
 //With fahrenheit having a value of 50.0, this would print 10.0.
