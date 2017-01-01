fbd89766dcc77ab0f544e847c06d21f9fa4bac83
Liza :) <lizash605@gmail.com>
2017-01-01 22:59:11 +0200

    Create full name.java

---
+++ PSet 1/full name.java
@@ -0,0 +1,6 @@
+String firstName = "Liza";
+String lastName = "S";
+String fullName = firstName + " " + lastName;
+System.out.println("Hello, my name is " + fullName + ".");
+int lettersInName = firstName.length() + lastName.length();
+System.out.println("There are " + lettersInName + " letters in my name.");
